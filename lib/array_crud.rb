4ed04440b8fb75fefa9c0a41d22b12ecbcabca3f
Parker Williamson <parker.williamson@gmail.com>
2020-05-05 17:48:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,8 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
+  people = ["wow", "Parker", "Anna", "Britney"]
+  people.shift
 end
 
 def retrieve_element_from_index(array, index_number)
