8ea45e367310fe2fd2287d936316a5ed2328cfeb
Parker Williamson <parker.williamson@gmail.com>
2020-05-05 17:44:20 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   people = ["Logan", "Parker", "Anna", "Britney"]
-  people << "Kris"
+  people << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
