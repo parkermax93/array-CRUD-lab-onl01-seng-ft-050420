07bf37cc6e3d81ba9e5a9b72cf9f358107a69c60
Parker Williamson <parker.williamson@gmail.com>
2020-05-05 17:50:29 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -33,7 +33,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  people = ["Logan", "Parker", "Anna", "Britney"]
+  people = ["wow", "Parker", "Anna", "Britney"]
   people[0]
 end
 
