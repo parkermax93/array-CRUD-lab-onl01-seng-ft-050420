d2b8a878590be7a6969e91641b799b7e0a974322
Parker Williamson <parker.williamson@gmail.com>
2020-05-05 17:46:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  people = ["Logan", "Parker", "Anna", "Britney"]
+  people.pop
 end
 
 def remove_element_from_start_of_array(array)
