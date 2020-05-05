74e924f41d105082d8ff6376d9e80448114487cf
Parker Williamson <parker.williamson@gmail.com>
2020-05-05 17:40:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -2,8 +2,8 @@ def create_an_empty_array
   []
 end
 
-def create_an_array
-  [Logan, Parker, Anna, Britney]
+def create_an_array[Logan, Parker, Anna, Britney]
+  
 end
 
 def add_element_to_end_of_array(array, element)
